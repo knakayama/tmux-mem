9584ca7c9e2e2cd844694ff1e27fa11e2a4ff2d6
knakayama <knakayama.sh@gmail.com>
2015-04-01 20:33:18 +0900

    change regexp

--- scripts/mem.sh
+++ scripts/mem.sh
@@ -3,7 +3,7 @@
 arch="$(uname -s)"
 
 if [[ "$arch" = "Linux" ]]; then
-    free -m | perl -awlne 'printf "Mem:%sM,%sM\n",$F[2],$F[3] if $_ =~ "buffers/cache"'
+    free -m | perl -awlne 'printf "Mem:%sM,%sM\n",$F[2],$F[3] if /buffers\/cache/'
 elif [[ "$arch" = "Darwin" ]]; then
     top -l 1 | head -n 10 | perl -awlne 'printf "Mem:%s,%s\n",$F[1],$F[5] if /^PhysMem/'
 else
