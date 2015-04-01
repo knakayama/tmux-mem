49f73aebda6aaceca55fb4eeb65f63e56ad0dcaf
knakayama <knakayama.sh@gmail.com>
2015-04-01 19:56:53 +0900

    add bash specific func

--- scripts/mem.sh
+++ scripts/mem.sh
@@ -2,7 +2,7 @@
 
 PLATFORM="$(uname)"
 
-if [ "$PLATFORM" = "Linux" ]; then
+if [[ "$PLATFORM" = "Linux" ]]; then
     free -m | perl -awlne 'printf "Mem:%sM,%sM\n",$F[2],$F[3] if $_ =~ "buffers/cache"'
 else
     echo "Mem:Unkonw platform"
