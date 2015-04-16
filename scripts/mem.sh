1df9b77edaa46e40411a2de9e9cda222a1b6bc76
knakayama <knakayama.sh@gmail.com>
2015-04-16 21:57:27 +0900

    swap

--- scripts/mem.sh
+++ scripts/mem.sh
@@ -5,7 +5,7 @@ arch="$(uname -s)"
 if [[ "$arch" = "Linux" ]]; then
     free -m | perl -awlne 'printf "Mem:%sM,%sM\n",$F[2],$F[3] if /buffers\/cache/'
 elif [[ "$arch" = "Darwin" ]]; then
-    top -l 1 | head -n 10 | perl -awlne 'printf "Mem:%s,%s\n",$F[1],$F[5] if /^PhysMem/'
+    top -l 1 | head -n 10 | perl -awlne 'printf "Mem:%s,%s\n",$F[5],$F[1] if /^PhysMem/'
 else
     echo "Mem:Unknown platform"
 fi
