3a731af65807f32237b0962c5b40e24690d9d4ef
knakayama <knakayama.sh@gmail.com>
2015-09-30 00:49:45 +0900

    Change MB to GB

--- scripts/mem.sh
+++ scripts/mem.sh
@@ -1,29 +1,33 @@
 #!/usr/bin/env bash
 
-arch="$(uname -s)"
+case "$(uname -s)" in
+  Linux)
+    free -m | awk '
+      /buffers\/cache/ {printf "Mem:%2.2fG,%2.2fG\n", $3 / 1024, $4 / 1024}
+    '
+    ;;
+  Darwin)
+    vm_stat | sed 's/\.$//' | awk '
+      /^(Pages (wired down|(in)?active|speculative)|Pages occupied by compressor)/ {
+        used_memory_with_cache += $NF
+      }
 
-if [[ "$arch" = "Linux" ]]; then
-  free -m | perl -awlne 'printf "Mem:%sM,%sM\n",$F[2],$F[3] if /buffers\/cache/'
-elif [[ "$arch" = "Darwin" ]]; then
-  vm_stat | sed 's/\.$//' | awk '
-  /^(Pages (wired down|(in)?active|speculative)|Pages occupied by compressor)/ {
-    used_memory_with_cache += $NF
-  }
+      /^(Pages purgeable|File-backed pages)/ {
+        cached_memory += $NF
+      }
 
-  /^(Pages purgeable|File-backed pages)/ {
-    cached_memory += $NF
-  }
+      /^Pages free/ {
+        free_memory += $NF
+      }
 
-  /^Pages free/ {
-    free_memory += $NF
-  }
-
-  END {
-    used_memory_mega_bytes = ((used_memory_with_cache - cached_memory) * 4096) / 1024 / 1024
-    free_memory_mega_bytes = ((cached_memory + free_memory) * 4096) / 1024 / 1024
-    printf("Mem:%dM,%dM\n", used_memory_mega_bytes, free_memory_mega_bytes)
-  }
-  '
-else
-  echo "Mem:Unknown platform"
-fi
+      END {
+        used_memory_mega_bytes = ((used_memory_with_cache - cached_memory) * 4096) / 1024 / 1024 / 1024
+        free_memory_mega_bytes = ((cached_memory + free_memory) * 4096) / 1024 / 1024 / 1024
+        printf("Mem:%2.2fG,%2.2fG\n", used_memory_mega_bytes, free_memory_mega_bytes)
+      }
+    '
+    ;;
+  *)
+    echo "Mem:Unknown platform"
+    ;;
+esac
